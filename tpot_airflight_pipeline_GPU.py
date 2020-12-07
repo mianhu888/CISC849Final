525d15675516d1692512b4018e14f4931ed0f022
Eric Zhang <57052113+mianhu888@users.noreply.github.com>
2020-12-07 16:42:20 -0500

    Add files via upload

---
+++ tpot_airflight_pipeline_GPU.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
+from sklearn.impute import SimpleImputer
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=42)
+
+imputer = SimpleImputer(strategy="median")
+imputer.fit(training_features)
+training_features = imputer.transform(training_features)
+testing_features = imputer.transform(testing_features)
+
+# Average CV score on the training set was: 0.9274632960811117
+exported_pipeline = XGBClassifier(alpha=10, learning_rate=1.0, max_depth=8, min_child_weight=6, n_estimators=100, nthread=1, subsample=0.8, tree_method="gpu_hist")
+# Fix random state in exported estimator
+if hasattr(exported_pipeline, 'random_state'):
+    setattr(exported_pipeline, 'random_state', 42)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
